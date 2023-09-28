39b14f99f1ea5b85f76bf855ed72512558b8bfb6
jeongsangtae <wjdtkdxo55@naver.com>
2023-09-28 16:46:53 +0900

    React Complete Training Form User Input Deepening - form submit handler add & input handler add (BasicForm component edit)

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,21 +1,64 @@
+import React, { useState } from "react";
+
 const BasicForm = (props) => {
+  const [enteredFirstName, setEnteredFirstName] = useState("");
+  const [enteredLastName, setEnteredLastName] = useState("");
+  const [enteredEmail, setEnteredEmail] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enteredFirstName, enteredLastName, enteredEmail);
+
+    setEnteredFirstName("");
+    setEnteredLastName("");
+    setEnteredEmail("");
+  };
+
+  const firstNameChangeHandler = (event) => {
+    setEnteredFirstName(event.target.value);
+  };
+
+  const lastNameChangeHandler = (event) => {
+    setEnteredLastName(event.target.value);
+  };
+
+  const emailNameChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="control-group">
+        <div className="form-control">
+          <label htmlFor="firstName">First Name</label>
+          <input
+            value={enteredFirstName}
+            type="text"
+            id="firstName"
+            onChange={firstNameChangeHandler}
+          />
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            value={enteredLastName}
+            type="text"
+            id="lastName"
+            onChange={lastNameChangeHandler}
+          />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="email">E-Mail Address</label>
+        <input
+          value={enteredEmail}
+          type="email"
+          id="email"
+          onChange={emailNameChangeHandler}
+        />
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
